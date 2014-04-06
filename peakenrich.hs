f8a40589cb6d980366eb9dde3ae9d249ba84c76c
tbepler <tbepler@gmail.com>
2014-04-06 19:42:11 -0400

    worked out some bugs in the peak classifying function. need to add overlap parameter as command line argument

--- peakenrich.hs
+++ peakenrich.hs
@@ -3,7 +3,7 @@
 
 --author: Tristan Bepler (tbepler@gmail.com)
 
---Usage: peakenrich -s/--seqs=FILE -g/--genome=FILE PEAK_FILES...
+--Usage: peakenrich -s/--seqs=FILE -g/--genome=FILE NAME=PEAK_FILE...
 
 import System.Console.GetOpt
 import System.Environment
@@ -39,13 +39,13 @@ parse argv = case getOpt Permute flags argv of
 	(_,_,errs) -> do
 			hPutStrLn stderr (concat errs ++ usageInfo header flags)
 			exitWith (ExitFailure 1)
-	where header = "Usage: peakenrich -s/--seqs=FILE -g/--genome=FILE PEAK_FILES..."
+	where header = "Usage: peakenrich -s/--seqs=FILE -g/--genome=FILE NAME=PEAK_FILE..."
 
 --peak = (classification, chromosome, start, end)
 type Peak = (String, String, Int, Int)
 
 classification :: Peak -> String
-classification (c, _, _, _) -> c
+classification (c, _, _, _) = c
 
 chromosome :: Peak -> String
 chromosome (_, c, _, _) = c
@@ -58,7 +58,7 @@ end (_, _, _, e) = e
 
 --returns a mapping of chromosome to all peaks on that chromosome
 parsePeaks :: String -> String -> Map.Map String [Peak]
-parsePeaks file classification = parse' (lines file) Map.empty where
+parsePeaks classification file = parse' (lines file) Map.empty where
 	parse' [] m = m
 	parse' (x:xs) m = parse' xs m' where
 		p = parse'' $ words x
@@ -69,21 +69,35 @@ parsePeaks file classification = parse' (lines file) Map.empty where
 classify :: [Map.Map String [Peak]] -> Map.Map String [Peak]
 classify [] = Map.empty
 classify [x] = x
+--classify xs = foldl combine Map.empty xs where
 classify xs = classify' (foldl combine Map.empty xs) xs where
-	insert p m = Map.insert (chromosome p) (p:(fromMaybe [] $ Map.lookup (chromosome p) m)) m
-	removeAll ps m = foldl (flip remove) m ps
-	remove p m = Map.adjust (remove' p) (chromosome p) m
+
+	classify' :: Map.Map String [Peak] -> [Map.Map String [Peak]] -> Map.Map String [Peak]
+	classify' m [] = m
+	classify' m (y:ys) = classify' m' ys' where
+		(m', ys') = classify'' m y ys
+
+	classify'' :: Map.Map String [Peak] -> Map.Map String [Peak] -> [Map.Map String [Peak]] -> (Map.Map String [Peak], [Map.Map String [Peak]])
+	classify'' m y ys = (m', ys') where
+		(m', ys') = Map.fold (\a (m1, zs)-> foldl (\(m2, ws) p-> classify''' m2 p ws) (m1,zs) a) (m, ys) y
+
+	classify''' :: Map.Map String [Peak] -> Peak -> [Map.Map String [Peak]] -> (Map.Map String [Peak], [Map.Map String [Peak]])
+	classify''' m p ys = (m', ys') where
+		overlap = concatMap (overlapPeaks p) ys
+		ys' = map (removeAll overlap) ys
+		m' = Main.insert (joinPeaks (p:overlap)) m
+
+insert :: Peak -> Map.Map String [Peak] -> Map.Map String [Peak]
+insert p m = Map.insert (chromosome p) (p:(fromMaybe [] $ Map.lookup (chromosome p) m)) m
+
+removeAll :: [Peak] -> Map.Map String [Peak] -> Map.Map String [Peak]
+removeAll ps m = foldl (flip remove) m ps
+
+remove :: Peak -> Map.Map String [Peak] -> Map.Map String [Peak]
+remove p m = Map.adjust (remove' p) (chromosome p) m where
+	remove' :: Peak -> [Peak] -> [Peak]
 	remove' p [] = []
 	remove' p (x:xs) = if p == x then remove' p xs else x:(remove' p xs)
-	classify' m [] = m
-	classify' m (x:xs) = classify' m' xs' where
-		(m', xs') = classify'' m x xs
-	classify'' m x xs = (m', xs') where
-		(m', xs') = Map.fold (\a (m,xs)-> foldl (\(m, xs) p-> classify''' m p xs) (m,xs) a) (m, xs) m
-	classify''' m p xs = (m', xs') where
-		overlap = concatMap (overlapPeaks p) xs
-		xs' = map (removeAll overlap) xs
-		m' = insert (joinPeaks (p:overlap)) m
 
 expandPeakMap :: Map.Map String [Peak] -> [Peak]
 expandPeakMap m = Map.fold (\x y-> x ++ y) [] m
@@ -93,9 +107,9 @@ joinPeaks [] = error "No peaks to join"
 joinPeaks [(desc, chr, s, e)] = (desc ++ "_Unique", chr, s, e)
 joinPeaks xs = (desc, chr, s, e) where
 	(_, chr, s, e) = joinPeaks' xs
-	desc = init $ foldl (\x y-> x ++ "_" ++ y) "" $ nub $ sort $ map (\(d, _, _, _) -> d) xs
+	desc = tail $ foldl (\x y-> x ++ "_" ++ y) "" $ nub $ sort $ map (\(d, _, _, _) -> d) xs
 	joinPeaks' [p] = p
-	joinPeaks' (p:ps) = join' p $ joinPeaks ps where
+	joinPeaks' (p:ps) = join' p $ joinPeaks' ps where
 		join' (_, aChr, aSt, aEnd) (_, bChr, bSt, bEnd) = ("", aChr, nStart, nEnd) where
 			nStart = if aSt > bSt then aSt else bSt
 			nEnd = if aEnd < bEnd then aEnd else bEnd
@@ -105,7 +119,7 @@ overlapPeaks p m = filter (overlaps p) chromPeaks where
 	chromPeaks = fromMaybe [] $ Map.lookup (chromosome p) m
 
 overlaps :: Peak -> Peak -> Bool
-overlaps (_, _, as, ae) (_, _, bs, be) = (((fromIntegral overlap) / (fromIntegral peakspan)) >= 0.5) where
+overlaps (_, _, as, ae) (_, _, bs, be) = (((fromIntegral overlap) / (fromIntegral peakspan)) >= 0.1) where
 	overlap
 		| (as > bs) && (bs >= ae) = bs - ae + 1
 		| (bs > as) && (as >= be) = as - be + 1
@@ -122,16 +136,23 @@ combine x y = foldl insert' Map.empty keys where
 	combine' k = (fromMaybe [] $ Map.lookup k x) ++ (fromMaybe [] $ Map.lookup k y)
 	insert' m k = Map.insert k (combine' k) m
 
-
+parsePeakArg :: String -> (String, String)
+parsePeakArg arg = parsePeakArg' False ("", "") arg where
+	parsePeakArg' False (left, "") [] = (left, left)
+	parsePeakArg' False (left, right) (x:xs) = if x == '=' then parsePeakArg' True (left, right) xs else parsePeakArg' False (left++[x], right) xs
+	parsePeakArg' True (left, right) [] = (left, right)
+	parsePeakArg' True (left, right) (x:xs) = parsePeakArg' True (left, right++[x]) xs
+ 
 main = do
-	(seqfilepath, genomefilepath, peakfilepaths) <- getArgs >>= parse
+	(seqfilepath, genomefilepath, peakargs) <- getArgs >>= parse
 	--read the seqfile and extract the sequences column from the file
 	seqfile <- readFile seqfilepath
-	let seqs = Table.stringEntries . fromMaybe (Table.StringCol "Sequence" []) . Table.lookupCol "Sequence" . Table.columns . read seqfile
+	let seqs = Table.stringEntries $ fromMaybe (Table.StringCol "Sequence" []) $ Table.lookupCol "Sequence" $ Table.columns $ read seqfile
 	--read the genome lazily as a bytestring
 	genomefile <- BL.readFile genomefilepath
 	let genome = BL.unpack genomefile
 	--read in the peaks, classify them, and store them in a map by chromosome
-	peakfiles <- map (readFile) peakfilepaths
-	let peakmap = classify $ map (parsePeaks) peakfiles
+	let (peaknames, peakfilepaths) = unzip $ map (parsePeakArg) peakargs
+	peakfiles <- mapM (readFile) peakfilepaths
+	let peakmap = classify $ map (\(x,y)-> parsePeaks x y) $ zip peaknames peakfiles
 	putStrLn $ unlines $ map (\(s, xs)-> unwords (s:(map (show) xs))) $ Util.groupBy classification $ expandPeakMap peakmap
